f617c1a55c745d63051cab11b10ee7f5195ef131
Alexismireles32 <137922510+Alexismireles32@users.noreply.github.com>
2023-06-27 14:40:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import os
+import streamlit as st
+import pandas as pd
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
-import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.agents.agent_toolkits import create_vectorstore_agent, VectorStoreToolkit, VectorStoreInfo
 
 os.environ['OPENAI_API_KEY'] = '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'
 
-llm = OpenAI(temperature=0.5)  # Adjust temperature if needed
+llm = OpenAI(temperature=0.5)
 embeddings = OpenAIEmbeddings()
 
 loader = PyPDFLoader('Manual_iglesia.pdf')
+
 pages = loader.load_and_split()
-store = Chroma.from_documents(pages, embeddings, collection_name='Manual_iglesia')
+page_content = [page[1] for page in pages]  # Extraer el contenido de las páginas
+data = pd.DataFrame({'page_content': page_content})
+
+store = Chroma.from_documents(data['page_content'], embeddings, collection_name='Manual_iglesia')
 
 vectorstore_info = VectorStoreInfo(
     name="Manual_iglesia",
@@ -27,7 +32,7 @@ agent_executor = create_vectorstore_agent(
     toolkit=toolkit
 )
 st.title('🦜🔗 El manual de la Iglesia Adventista del Septimo Dia')
-prompt = st.text_input('¿Que pregunta tienes? Habla directamente con el manual y obtendras respuestas precisas y utiles! Consejo: Especifica que quieres saber el Capitulo y la pagina donde se encuentra la infromacion')
+prompt = st.text_input('¿Qué pregunta tienes? Habla directamente con el manual y obtendrás respuestas precisas y útiles. Consejo: Especifica el capítulo y la página donde se encuentra la información.')
 
 if prompt:
     response = agent_executor.run(f"{prompt}?")
